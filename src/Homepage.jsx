d7c2163af82b06692e6266fd59d51d6316c08b07
RKPH <pnghung2003@gmail.com>
2024-10-20 13:46:39 +0700

    testbutton  in homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
 import { increaseCount } from "shell/counterStore";
 
 const Homepage = () => {
-  const dispatch = useDispatch();
   return (
     <div>
       Hello Homepage Testting Fuck cần bull shit
