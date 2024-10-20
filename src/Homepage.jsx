e8a67f598113c08a38429648d4f0703ecc7582f8
RKPH <pnghung2003@gmail.com>
2024-10-20 13:55:08 +0700

    test funct  v2  in homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,6 +4,7 @@ import useCounterStore from "shell/counterStore"; // Correct import in remote
 
 const Homepage = () => {
   const increaseCount = useCounterStore((state) => state.increaseCount); // Get increaseCount action
+  console.log(increaseCount);
   return (
     <div>
       Hello Homepage Testting Fuck cần bull shit
