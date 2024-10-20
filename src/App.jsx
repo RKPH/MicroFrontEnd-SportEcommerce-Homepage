6cdb68648a075fa6d3c86167f8e004ffeb174cc1
RKPH <pnghung2003@gmail.com>
2024-10-20 14:58:30 +0700

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Homepage from './Homepage'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <Homepage />
-  )
+  return <Homepage />;
 }
 
-export default App
+export default App;
