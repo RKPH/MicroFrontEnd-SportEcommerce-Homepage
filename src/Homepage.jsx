77c393d15d0e8940e513b9a53b1e18fa9e1a82ac
RKPH <pnghung2003@gmail.com>
2024-10-24 21:44:42 +0700

    add next section in homepagw

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -6,25 +6,27 @@ const Homepage = () => {
   const count = useCounterStore((state) => state.count);
 
   return (
-    <div
-      className="w-full min-h-screen bg-auto bg-no-repeat relative"
-      style={{
-        backgroundImage: `url("https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/Homepage_banner_hero_alt_spec_generic_asset_d_1_01e19778e1.jpg")`,
-      }}
-    >
-      <button
-        className="border-black border-2 px-4 py-2 absolute top-5 left-5"
-        onClick={() => {
-          console.log("Increase Count Called");
-          increaseCount();
+    <main>
+      {/* Hero banner */}
+      <div
+        className="w-full min-h-screen bg-auto bg-no-repeat relative"
+        style={{
+          backgroundImage: `url("https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/Homepage_banner_hero_alt_spec_generic_asset_d_1_01e19778e1.jpg")`,
         }}
       >
-        Count +1
-      </button>
+        <button
+          className="border-black border-2 px-4 py-2 absolute top-5 left-5"
+          onClick={() => {
+            console.log("Increase Count Called");
+            increaseCount();
+          }}
+        >
+          Count +1
+        </button>
 
-      {/* Text Block */}
-      <div
-        className="
+        {/* Text Block */}
+        <div
+          className="
           absolute 
           bottom-5 left-5 
           md:bottom-52 md:left-32 
@@ -34,32 +36,42 @@ const Homepage = () => {
           md:items-start md:text-left 
           md:static md:flex-none
         "
-      >
-        <span className="text-lg tracking-wide font-[AdihausDIN] uppercase font-bold text-black bg-white px-2 py-1">
-          MEMBERS GET MORE
-        </span>
-        <span className="text-sm tracking-wide font-[AdihausDIN] text-black bg-white px-2 py-1 max-w-[350px] text-wrap">
-          Join adiClub for exclusive products, events, and prize draws during
-          adiClub Days starting 24 Oct.
-        </span>
+        >
+          <span className="text-lg tracking-wide font-[AdihausDIN] uppercase font-bold text-black bg-white px-2 py-1">
+            MEMBERS GET MORE
+          </span>
+          <span className="text-sm tracking-wide font-[AdihausDIN] text-black bg-white px-2 py-1 max-w-[350px] text-wrap">
+            Join adiClub for exclusive products, events, and prize draws during
+            adiClub Days starting 24 Oct.
+          </span>
 
-        {/* Optional: Add buttons for free join and explore */}
-        <div className="flex gap-4 mt-2">
-          <button className="bg-white text-black px-4 py-2 uppercase W-1/2">
-            Join for free{" "}
-            <span>
-              <TrendingFlatIcon />
-            </span>
-          </button>
-          <button className="bg-white text-black border-2 px-4 py-2 uppercase W-1/2">
-            Explore more{" "}
-            <span>
-              <TrendingFlatIcon />
-            </span>
-          </button>
+          {/* Optional: Add buttons for free join and explore */}
+          <div className="flex gap-4 mt-2">
+            <button className="bg-white text-black px-4 py-2 uppercase W-1/2">
+              Join for free{" "}
+              <span>
+                <TrendingFlatIcon />
+              </span>
+            </button>
+            <button className="bg-white text-black border-2 px-4 py-2 uppercase W-1/2">
+              Explore more{" "}
+              <span>
+                <TrendingFlatIcon />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div>
+        <ul className="flex gap-x-4">
+          <li>DropSet</li>
+          <li>Adizero</li>
+          <li>New Arrivals</li>
+          <li>Takewondo</li>
+        </ul>
+      </div>
+    </main>
   );
 };
 
