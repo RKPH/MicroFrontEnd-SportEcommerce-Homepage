c34e3279bead37eef636add33ba8b81cc4140153
RKPH <pnghung2003@gmail.com>
2024-10-30 23:35:03 +0700

    fix bg imgae home

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -58,45 +58,9 @@ const Homepage = () => {
   return (
     <main className="w-full h-full">
       {/* Hero banner */}
-      <div className="w-full">
-        <img
-          src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/Homepage_banner_hero_alt_spec_generic_asset_d_1_01e19778e1.jpg"
-          className="h-[850px] w-full relative"
-        />
-        <button
-          className="border-black border-2 px-4 py-2 absolute top-5 left-5"
-          onClick={() => {
-            console.log("Increase Count Called");
-            increaseCount();
-          }}
-        >
-          Count +1
-        </button>
-
-        {/* Text Block */}
-        <div className="absolute bottom-5 left-5 md:bottom-52 md:left-32 flex flex-col gap-2 items-start justify-center text-center md:items-start md:text-left md:static md:flex-none">
-          <span className="text-lg tracking-wide font-[AdihausDIN] uppercase font-bold text-black bg-white px-2 py-1">
-            MEMBERS GET MORE
-          </span>
-          <span className="text-sm tracking-wide font-[AdihausDIN] text-black bg-white px-2 py-1 max-w-[350px] text-wrap">
-            Join adiClub for exclusive products, events, and prize draws during
-            adiClub Days starting 24 Oct.
-          </span>
-
-          <div className="flex gap-4 mt-2">
-            <button className="bg-white text-black px-4 py-2 uppercase">
-              Join for free <TrendingFlatIcon />
-            </button>
-            <button className="bg-white text-black border-2 px-4 py-2 uppercase ">
-              Explore more <TrendingFlatIcon />
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div>
+      <div className="w-full my-2">
         {/* Categories Navigation */}
-        <ul className="flex gap-x-4 w-full bg-white px-5 font-[AdihausDIN] text-base font-bold">
+        <ul className="flex gap-x-4 w-full bg-white px-5 font-[AdihausDIN] text-base font-bold ">
           {categories.map((category) => (
             <li
               key={category}
