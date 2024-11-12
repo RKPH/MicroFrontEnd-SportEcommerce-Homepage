09a33c0b6127b6171cfe496125341e8ebba2b97a
RKPH <pnghung2003@gmail.com>
2024-11-12 23:11:28 +0700

    add  Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route
-import Homepage from "./Homepage";
+import Homepage from "./Homepage"; // Import your Homepage component
 import "./App.css";
 
 function App() {
-
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
+      {/* Ensure all routes are inside the Router */}
       <Routes>
-        {/* Use the element prop to render components */}
         <Route path="/" element={<Homepage />} />
       </Routes>
     </Router>
