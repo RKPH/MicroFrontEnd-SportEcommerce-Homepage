39b659d44326e43772dee4622fe20eada5cad604
RKPH <pnghung2003@gmail.com>
2024-11-12 23:08:07 +0700

    add  Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route
 import Homepage from "./Homepage";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <Homepage />;
+
+  return (
+    <Router>
+      <Routes>
+        {/* Use the element prop to render components */}
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
