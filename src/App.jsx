bf17bb3e7bf986ff466135fdb237f42a7e18a2e2
RKPH <pnghung2003@gmail.com>
2024-11-12 23:20:09 +0700

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes and Route
+
+
 import Homepage from "./Homepage"; // Import your Homepage component
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <Router>
-      {/* Ensure all routes are inside the Router */}
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </Router>
-  );
+  return <Homepage />;
 }
 
 export default App;
